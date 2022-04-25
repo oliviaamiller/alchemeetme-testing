cd2992353823d5ec9dc9d9626d50e0eefcbd1c12
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-04-25 15:53:03 -0700

    render username test passing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,18 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-it('Should render the header', () => {
+const user = {
+  id: 1,
+  created_at: '2021-12-13T00:17:29+00:00',
+  name: 'Vonta',
+  avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
+  header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
+  likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
+  motto: 'Res Non Verba',
+  color: 'crimson',
+}
 
-    render(
-        <MemoryRouter>
-            <App />
-        </MemoryRouter>
-    );
+describe('first', () => {
+  it('Should render the header', async () => {
+    render(<App user={user} />)
 
-    screen.getAllByAltText('Alchemy Logo');
+    const alt = screen.getByAltText('Alchemy Logo')
 
+    const name = await screen.findByText('Vonta')
 
-
-});
-
+    expect(alt).toBeInTheDocument()
+    expect(name).toBeInTheDocument()
+  })
+})
